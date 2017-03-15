0926a09863bfd1db9818e6ee9e7812138cec936c
Max Macdonald <C15740661@mydit.ie>
2017-03-15 13:18:56 +0000

    Setup

---
+++ src/ie/dit/SolarWonders.java
@@ -0,0 +1,27 @@
+package ie.dit;
+
+import processing.core.PApplet;
+
+public class SolarWonders extends PApplet
+{
+	public void setup()
+	{
+
+	}
+	
+	public void draw()
+	{
+		
+	}
+	
+	public void settings()
+	{
+		size(1024, 512);
+	}
+	
+    public static void main(String[] args)
+    {
+        PApplet.main("ie.dit.SolarWonders");
+    }
+
+}
