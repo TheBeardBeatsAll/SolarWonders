fbe9a26099e90d527e5c778aa447b8c2db047765
Max Macdonald <C15740661@mydit.ie>
2017-03-21 12:17:51 +0000

    Learning 3D

--- src/ie/dit/SolarWonders.java
+++ src/ie/dit/SolarWonders.java
@@ -4,24 +4,32 @@ import processing.core.PApplet;
 
 public class SolarWonders extends PApplet
 {
+	float x,y,z; 
+	
 	public void setup()
 	{
-
+		background(0);
+		x = width/2;
+		y = height/2;
+		z = 0;
 	}
 	
 	public void draw()
 	{
-		
+		translate(x,y,z);
+		rotateX(PI/8);
+		rotateY(PI/8);
+		rectMode(CENTER);
+		rect(0,0,100,100);
 	}
 	
 	public void settings()
 	{
-		size(1024, 512);
+		size(1024, 512, P3D);
 	}
 	
     public static void main(String[] args)
     {
         PApplet.main("ie.dit.SolarWonders");
     }
-
 }
