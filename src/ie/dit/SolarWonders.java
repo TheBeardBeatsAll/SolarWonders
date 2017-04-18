44cf2e63db484d9505b19864e0fb1fd096cf0480
FinnMcLaughlin <C15536837@mydit.ie>
2017-04-18 16:33:04 +0100

    Texture choice function started

--- src/ie/dit/SolarWonders.java
+++ src/ie/dit/SolarWonders.java
@@ -1,14 +1,19 @@
 package ie.dit;
 
+import java.awt.Image;
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.core.PFont;
 import processing.core.PImage;
 
 public class SolarWonders extends PApplet
 {
-	Planet mover;
+ 	Planet mover;
 	Menu menu;
+	Textures tex;
 	PImage red, blue, coarse;
+	PImage[] imgs = new PImage[15];
 	PFont font;
 	boolean check = false;
 
@@ -16,14 +21,22 @@ public class SolarWonders extends PApplet
 	public void setup()
 	{
 		smooth();
-		red = loadImage("Planet_text1.png");
+
+		imgs[0] = loadImage("red.png");
+		imgs[1] = loadImage("blue.png");
+		imgs[2] = loadImage("coarse.jpg");
+		
+		
+		/*red = loadImage("red.png");
 		blue = loadImage("blue.png");
-		coarse = loadImage("coarse.jpg");
+		coarse = loadImage("coarse.jpg");*/
+		
 		font = createFont("font.ttf", 25);
+		
 		// Make Mover object
 		mover = new Planet(this);
 		menu = new Menu(this, red, blue, coarse, font);
-		  
+		tex = new Textures(this, imgs);
 	}
 	
 	public void draw()
@@ -44,7 +57,8 @@ public class SolarWonders extends PApplet
 	public void Finn()
 	{
 		background(45, 20, 55);
-		menu.menu();
+		//menu.menu();
+		tex.planet();
 	}
 	
 	public void settings()
